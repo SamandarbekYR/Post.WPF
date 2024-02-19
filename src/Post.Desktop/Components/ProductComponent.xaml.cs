421b3643fa6f3167b9fca5de4ae9abe1c935853c
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-02-19 09:32:10 +0500

    Created productComponent

---
+++ src/Post.Desktop/Components/ProductComponent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Post.Desktop.Components;
+
+/// <summary>
+/// Interaction logic for ProductComponent.xaml
+/// </summary>
+public partial class ProductComponent : UserControl
+{
+    public ProductComponent()
+    {
+        InitializeComponent();
+    }
+    private void Border_MouseEnter(object sender, MouseEventArgs e)
+    {
+        product_Border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ECECEC"));
+    }
+
+    private void Border_MouseLeave(object sender, MouseEventArgs e)
+    {
+        product_Border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Transparent"));
+    }
+
+    private void Delete_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+}
